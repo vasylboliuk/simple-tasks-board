6b5cda2011bc69e618e64f9429cbfd72d5595cc7
Vasyl Boliuk <vbboliuk@gmail.com>
2025-04-30 11:41:10 +0300

    fix: Refactor main file

--- src/main.py
+++ src/main.py
@@ -21,6 +21,29 @@ app = FastAPI()
 app.include_router(version_router, prefix="/api")
 
 
+def load_infisical_secrets():
+    """Load Infisical secrets."""
+    logging.info("Setup Authentication...")
+    infisical_settings = InfisicalSettings()
+    path = "/"
+    infisical_client = InfisicalClient.load_from_settings(infisical_settings)
+    infisical_client.get_secrets(path)
+
+
+def setup_logger():
+    """Setup logger."""
+    sys.path.append(str(CommonPaths.project_root))
+    LoggingManager.setup_logger()
+
+
+def start_application(settings: Settings):
+    """Start application."""
+    logging.info("Starting application...")
+    uvicorn.run(
+        app, host=settings.host, port=settings.port, log_config=None
+    )  # log_config=None enable own custom logs for this lib
+
+
 def main():
     """Main method represents entry point for start application.
 
@@ -29,19 +52,10 @@ def main():
     os.makedirs(CommonPaths.log_path, exist_ok=True)
     load_dotenv()
     settings = Settings()
-    sys.path.append(str(CommonPaths.project_root))
-    LoggingManager.setup_logger()
 
-    logging.info("Setup Authentication...")
-    infisical_settings = InfisicalSettings()
-    path = "/"
-    infisical_client = InfisicalClient.load_from_settings(infisical_settings)
-    infisical_client.get_secrets(path)
-
-    logging.info("Starting application...")
-    uvicorn.run(
-        app, host=settings.host, port=settings.port, log_config=None
-    )  # log_config=None enable own custom logs for this lib
+    setup_logger()
+    load_infisical_secrets()
+    start_application(settings)
 
 
 if __name__ == "__main__":
